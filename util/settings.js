6b60f175ceb7c823010da2684201e0ea5da70ddd
Mikolertesx <mgangsal@outlook.com>
2020-05-12 23:03:41 -0500

    Added even more console information

--- util/settings.js
+++ util/settings.js
@@ -1,5 +1,6 @@
+console.log('ENTORNO', process.env);
+console.log('Fase', process.env.NODE_ENV);
 module.exports.PORT = process.env.PORT || 3000;
 module.exports.SECRET = process.env.SECRET || 'JUST-MONIKA';
 module.exports.STAGE = process.env.NODE_ENV || 'development';
-module.exports.SSL = process.env.NODE_ENV ? true: false;
 module.exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/easy-verse';
