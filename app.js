418ce8094eb405aede635c81700a4d32946b9fd2
Mikolertesx <mgangsal@outlook.com>
2020-05-12 22:05:47 -0500

    First test database

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const settings = require('./util/settings');
 const app = express();
 
+// Postgre-promise.
+const pgp = require('pg-promise')();
+const db = pgp(settings.DATABASE_URL);
 
+db.result()
+  .then(result => {
+    console.log(result);
+  });
+
+//TODO Add a database setup.
+//TODO Add a bundler.
+//TODO Add a different
 
 app.listen(settings.Port);
--- util/settings.js
+++ util/settings.js
@@ -1,2 +1,5 @@
 module.exports.PORT = process.env.PORT || 3000;
 module.exports.SECRET = process.env.SECRET || 'JUST-MONIKA';
+module.exports.STAGE = process.env.NODE_ENV || 'development';
+module.exports.SSL = process.env.NODE_ENV ? true: false;
+module.exports.DATABASE_URL = process.env.DATABASE_URL || '';
