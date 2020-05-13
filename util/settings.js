1e97f07c95cc1914f159fb8b68273fed466726af
Mikolertesx <mgangsal@outlook.com>
2020-05-12 22:49:09 -0500

    Updated settings to reflect Heroku.

--- util/settings.js
+++ util/settings.js
@@ -2,4 +2,4 @@ module.exports.PORT = process.env.PORT || 3000;
 module.exports.SECRET = process.env.SECRET || 'JUST-MONIKA';
 module.exports.STAGE = process.env.NODE_ENV || 'development';
 module.exports.SSL = process.env.NODE_ENV ? true: false;
-module.exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/easy-verse';
+module.exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/easy-verse';
